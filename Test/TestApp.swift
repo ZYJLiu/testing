44e245086b40b0ecd60507df88ee9dd193d04ff8
John <johnzhengyuliu@gmail.com>
2023-01-23 20:09:41 -0600

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by John Liu on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
